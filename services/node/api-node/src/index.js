4885ff0af77dfec191b2138a624cef34d6c0ff0f
Thai Nong <hoangthai9217@gmail.com>
2026-01-19 16:53:59 +0700

    feat(feature/update-service-prefixes): update go and node path prefixes

--- services/node/api-node/src/index.js
+++ services/node/api-node/src/index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 // setup the logger
 app.use(morgan("tiny"));
 
-app.get("/", async (req, res) => {
+// Routes with /api/node prefix
+app.get("/api/node", async (req, res) => {
   await insertRequest();
   const response = await getDateTimeAndRequests();
   console.log = response;
@@ -17,8 +18,12 @@ app.get("/", async (req, res) => {
   res.send(response);
 });
 
-app.get("/ping", async (_, res) => {
+app.get("/api/node/ping", async (_, res) => {
   res.send("pong");
+});
+
+app.get("/api/node/health", async (_, res) => {
+  res.send("ok");
 });
 
 const server = app.listen(port, () => {
